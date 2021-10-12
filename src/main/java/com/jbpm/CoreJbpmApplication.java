5983617f7c4b2c24552709b8a5aa89badaeb3aa1
Albin Jenith M <albinjenith@gmail.com>
2021-10-12 18:35:34 +0530

    Initial

---
+++ src/main/java/com/jbpm/CoreJbpmApplication.java
@@ -0,0 +1,13 @@
+package com.jbpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJbpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJbpmApplication.class, args);
+	}
+
+}
